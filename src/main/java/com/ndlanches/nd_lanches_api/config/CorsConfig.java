ec0b60f72261be7b6e4c44788695779a5ac26af0
Arthur Amancio Francisco <arthur.francisco@ubfer.local>
2026-03-17 10:40:24 -0300

    Adicionando configuração de CORS

---
+++ src/main/java/com/ndlanches/nd_lanches_api/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ndlanches.nd_lanches_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Libera o acesso para o seu frontend no Firebase e testes locais
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                    "https://ndlanches-e7ccf.web.app", 
+                    "https://ndlanches-e7ccf.firebaseapp.com",
+                    "http://localhost:5500",
+                    "http://127.0.0.1:5500"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
